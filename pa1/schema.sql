5135ab8b39c1dd3309b2790a9563d5144bf718c2
LevkoBe <lbeniakh@kse.org.ua>
2024-04-12 12:03:36 +0300

    restructuring

--- pa1/schema.sql
+++ pa1/schema.sql
@@ -5,8 +5,6 @@ SELECT title, rating
     FROM books
     ORDER BY rating DESC
     LIMIT 10;
--- time (ms): 6, 4, 4, 4, 4, 5, 2, 5, 5, 7, 21
--- avg: 6.1
 
 -- #### Selecting the best authors based on the rating of their books
 -- For users to be able to review the authors starting from those whose books' ratings are the highest.
@@ -18,8 +16,6 @@ SELECT a.name author, AVG(b.rating) average_rating FROM books b
     GROUP BY a.id
     ORDER BY average_rating DESC
     LIMIT 10;
--- time (ms): 8, 4, 6, 5, 5, 5, 5, 4, 4, 5
--- avg: 5.1
 
 -- #### Selecting the most active customers based on the number of loans they have
 -- May be used to provide monthly reviews or everyday leaderboards for users.
@@ -31,8 +27,6 @@ SELECT c.name customer, COUNT(DISTINCT l.id) loans_n, COUNT(DISTINCT b.id) books
     GROUP BY c.id
     ORDER BY loans_n DESC
     LIMIT 10;
--- time (ms): 17, 5, 2, 6, 4, 4, 8, 9, 6, 8
--- avg: 6.9
 
 -- #### Selecting 10 most commonly borrowed books
 -- May be used in advertising, articles, and in other ways. Helps promote, or popularize some books, or prompt customers to buy books.
@@ -42,8 +36,6 @@ SELECT COUNT(lb.loan_id) loans_number, b.title title FROM books b
     GROUP BY b.id
     ORDER BY loans_number DESC
     LIMIT 10;
--- time (ms): 8, 5, 8, 8, 8, 5, 4, 3, 17, 6
--- avg: 7.2
 
 -- #### Selecting users with the highest number of non-returned loans
 -- May be used for creating special lists of users that may need to be paid more attention, or require some additional notifications.
@@ -56,8 +48,6 @@ SELECT c.name customer, COUNT(DISTINCT l.id) loans_number, COUNT(DISTINCT b.id) 
     HAVING MAX(l.return_date) IS NULL
     ORDER BY non_returned_books DESC
     LIMIT 10;
--- time (ms): 5, 6, 9, 5, 5, 8, 5, 5, 6, 5
--- avg: 5.9
 
 
 -- #### Selecting users with the highest number of returned loans
@@ -70,8 +60,6 @@ SELECT c.name customer, COUNT(DISTINCT l.id) loans_returned, COUNT(DISTINCT b.id
     GROUP BY c.id
     ORDER BY loans_returned DESC
     LIMIT 10;
--- time (ms): 8, 5, 4, 5, 4, 4, 4, 4, 5, 5
--- avg: 4.8
 
 -- ### Find the Most Popular Genre by Count of Books Sold:
 -- This information can help bookstore owners or publishers understand which genres are in high demand among readers,
@@ -86,9 +74,6 @@ SELECT g.name AS genre, COUNT(*) AS loans_count
     GROUP BY g.name
     ORDER BY loans_count DESC
     LIMIT 10;
--- time (ms): 15, 23, 7, 5, 8, 5, 12, 5, 5, 6
--- avg: 9.1
-
 
 
 -- ### Calculate the Average Loan Duration for Each Genre:
@@ -103,8 +88,6 @@ SELECT g.name AS genre, AVG(DATEDIFF(l.return_date, l.loan_date)) AS avg_loan_du
     JOIN loans l ON lb.loan_id = l.id
     WHERE l.return_date IS NOT NULL
     GROUP BY g.name;
--- time (ms): 8, 4, 7, 6, 7, 5, 4, 6, 4, 5
--- avg: 5.6
 
 -- ### Currently available books
 -- Is needed for showing customers what books can be borrowed, and people who have the borrowed ones
@@ -124,10 +107,4 @@ SELECT
         LEFT JOIN  loans l ON lb.loan_id = l.id
         LEFT JOIN customers c ON l.customer_id = c.id
     ORDER BY b.title;
--- time (ms): 5, 20, 5, 5, 5, 4, 5, 4, 6, 4
--- avg: 6.3
-
--- avgs: 6.3, 5.6, 9.1, 4.8, 5.9, 7.2, 6.9, 5.1, 6.1
--- aavg: 6.33
-
--- Approximate speedup: 18%
+    
